b36d18f9142fd584a5add44c53fb08c80259ec55
Jack S. Hale <mail@jackhale.co.uk>
2024-09-06 17:22:39 +0200

    Update image to latest Jupyterlab and jupyter_rise.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -v $(pwd):/opt/notebooks -i -t -p 8888:8888 jhale/scientific-python-course /bin/bash -c "mkdir -p /opt/notebooks && \
-  /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser"
+docker run -v $(pwd):/opt/notebooks -i -t -p 8888 jhale/scientific-python-course /bin/bash -c "mkdir -p /opt/notebooks && \
+  /opt/conda/bin/jupyter lab --allow-root --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser"
